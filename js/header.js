7da5ec391e9392c8d9e7a0f99fb6289e7747184f
PO <gashpofigma@gmail.com>
2025-02-19 17:00:24 +0800

    02/19 DQA - 2, 5, 7

--- js/header.js
+++ js/header.js
@@ -3,12 +3,48 @@ $(document).ready(function () {
     $("header").toggleClass("open");
   });
 
-  $(".header-dropdown").click(function (event) {
+  $("#user").click(function (event) {
     event.stopPropagation();
-    if ($(window).width() > 768) {
+    if ($(window).width() >= 768) {
       $(".select").toggleClass("active");
     } else {
-      $(".mobile-frame").toggleClass("open");
+      if ($("#mobile-menu").length === 0) {
+        $("body").append(
+          `
+          <div id="mobile-menu">
+            
+
+            <div class="container">
+              <div class="balance">
+                <img src="images/Logo_Coin.svg">
+                <h6>9,999</h6>
+              </div>
+              <ul class="mobile-menu">
+                <li onclick="location.href='myPoints.html'"><i class="material-symbols-outlined">receipt_long</i>我的紅利</li>
+                <li onclick="location.href='myTickets.html'"><i class="material-symbols-outlined">folder_open</i>我的票夾</li>
+                <li class="divider"></li>
+                <li><i class="material-symbols-outlined">person</i>會員資料</li>
+                <li onclick="location.href='index.html'"><i class="material-symbols-outlined">logout</i>登出</li>
+              </ul>
+            </div>
+
+            <footer>
+              <div class="info">
+                <div class="logo"><a href="index.html"><img src="images/Logo-white.svg"></a></div>
+                <ul class="menu">
+                  <li class="footer-textlink">隱私權政策</li>
+                  <li class="footer-textlink">服務條款</li>
+                  <li class="footer-textlink">紅利規章</li>
+                </ul>
+              </div>
+              <copyright>2025 GASH POINT Co., Ltd. All Rights Reserved.</copyright>
+            </footer>
+          </div>
+          `
+        );
+      } else {
+        $("#mobile-menu").remove();
+      }
     }
   });
 
@@ -27,10 +63,8 @@ $(document).ready(function () {
 
   // 小網隱藏header
   var prev = 0;
-
   $(window).on("scroll", function () {
     var scrollTop = $(window).scrollTop();
-
     if (scrollTop < 100) {
       $("header").removeClass("hide");
     } else {
